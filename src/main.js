8c48d2200be5b2e25f7ae75a6db90e4768b4175e
Alexandre Galiay <dev@alexandregaliay.com>
2020-10-16 00:04:50 +0200

    Remove Prismic integration & first try on style

--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 import Vue from 'vue';
-import App from './App.vue';
-import PrismicVue from '@prismicio/vue'
-import linkResolver from './js/link-resolver.js';
-import htmlSerializer from './js/html-serializer.js';
+import App from './App.vue'
 
-const accessToken = 'MC5YNFhQS3hFQUFDWUFIWG1m.77-977-9a0MM77-977-9JQ_vv71mUyJ5DRQk77-977-977-9K--_vRQr77-977-9Q--_vW82L--_vQ';
-
-Vue.use(PrismicVue, {
-  endpoint: "https://alexandregaliay-com.cdn.prismic.io/api/v2",
-  linkResolver: linkResolver,
-  htmlSerializer: htmlSerializer,
-  apiOptions: { accessToken }
-});
-
-new Vue({ render: createElement => createElement(App) }).$mount('#app');
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
