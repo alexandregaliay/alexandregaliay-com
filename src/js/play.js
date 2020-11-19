ec0a110c11cbb5a4444a97d4edfbca12ca2355e6
Alexandre Galiay <dev@alexandregaliay.com>
2020-11-19 01:17:26 +0100

    Improve playground

--- src/js/play.js
+++ src/js/play.js
@@ -1,9 +1,38 @@
 import Particles from './play/particles.js';
 import RetroGrid from './play/retrogrid.js';
 
-document.querySelector('footer a').addEventListener('click', function(e) {
+document.querySelector('footer a').addEventListener('click', event => {
+  document.body.classList.add('play');
   new Particles();
   new RetroGrid();
-  e.preventDefault;
+  insertExit();
+  event.preventDefault;
 });
 
+function insertExit() {
+  const exitTag = document.createElement('div');
+  exitTag.className = 'exit';
+  
+  const exitText = document.createElement('span');
+  exitText.innerText = 'back';
+  exitTag.append(exitText);
+
+  document.querySelector('footer').append(exitTag);
+
+  const keySequence = ['b', 'a', 'c', 'k'];
+  let userInput = new Array(keySequence.length);
+
+  window.addEventListener('keydown', ({ key }) => {
+    userInput = [ ...userInput.slice( 1 ), key ];
+    if (keySequence.every((v, k) => v === userInput[k])) {
+      deletePlayground();
+    }
+} );
+}
+
+function deletePlayground() {
+  document.body.classList.remove('play');
+  document.querySelector('.exit').remove();
+  document.querySelector('.particles').remove();
+  document.querySelector('.three-grid').remove();
+}
