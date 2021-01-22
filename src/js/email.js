02d1f84c107bc08f4f689e60f606a36a6a49c2d3
Alexandre Galiay <dev@alexandregaliay.com>
2021-01-23 00:45:44 +0100

    Refactor email function

--- src/js/email.js
+++ src/js/email.js
@@ -1,26 +1,27 @@
-// Email & tooltip
-const emailValue = 'hello@alexandregaliay.com';
-const emailLink = document.querySelector('.email');
-const tooltip = document.querySelector('.tooltip');
-const origText = tooltip.textContent;
+let emailValue = 'hello@alexandregaliay.com'
+let emailLink = document.querySelector('.email')
+const tooltip = document.querySelector('.tooltip')
+let origText = tooltip.textContent
 
-emailLink.addEventListener('click', copyEmail);
-emailLink.addEventListener('mouseover', updateTooltip);
-
-function copyEmail(event) {
-  const el = document.createElement('textarea');
-  el.value = emailValue;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-  tooltip.textContent = 'Copied';
-  setTimeout(updateTooltip, 3000);
-  event.preventDefault();
+function copyValue(event) {
+  const el = document.createElement('textarea')
+  el.value = emailValue
+  document.body.appendChild(el)
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
+  tooltip.textContent = 'Copied'
+  setTimeout(updateTooltip, 3000)
+  event.preventDefault()
 }
 
 function updateTooltip() {
   if(tooltip.textContent != origText) {
-    tooltip.textContent = origText;
+    tooltip.textContent = origText
   }
+}
+
+export default function emailCopy() {
+  emailLink.addEventListener('click', copyValue)
+  emailLink.addEventListener('mouseover', updateTooltip)
 }
