5ebec436e263d338fc2d51905a0bb9f8516a767a
Alexandre Galiay <dev@alexandregaliay.com>
2021-02-15 23:09:25 +0100

    Optimize JS

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,10 @@ triggerAnimation('footer')
 // Play
 document.querySelector('footer a').addEventListener('click', (event) => {
   if (!document.body.classList.contains('play')) {
+    document.body.classList.add('play')
+    triggerAnimation('#page-transition', false)
+    triggerAnimation('#wrapper', false)
+    triggerAnimation('footer', false)
     import('./play.js').then(function(page) {
       page.init()
     })
